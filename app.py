87ee19598734922547c2ceeff3e7b00341abc6fe
ManosV <manosv18@gmail.com>
2026-03-17 11:01:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -87,9 +87,9 @@ if st.session_state.baseline_locked:
         volume=float(s.get("volume", 1000)),
         variable_cost=float(s.get("variable_cost", 60)),
         fixed_cost=float(s.get("fixed_cost", 20000)),
-        ar_days=float(s.get("ar_days", 45)),
-        inv_days=float(s.get("inv_days", 60)),
-        ap_days=float(s.get("ap_days", 30)),
+        ar_days=int(s.get("ar_days", 45)),
+        inv_days=int(s.get("inv_days", 60)),
+        ap_days=int(s.get("ap_days", 30)),
         annual_debt_service=float(s.get("annual_debt_service", 0)),
         opening_cash=float(s.get("opening_cash", 10000)),
         target_profit=float(s.get("target_profit_goal", 0))
