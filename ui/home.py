b2a3cace85075f096a0e341598a439aaf369b52d
ManosV <manosv18@gmail.com>
2026-04-22 19:29:47 +0300

    Update home.py

--- ui/home.py
+++ ui/home.py
@@ -4,18 +4,6 @@ from datetime import datetime
 
 def run_home():
     s = st.session_state
-    # --- SURGICAL OVERRIDE ---
-    # Αυτό το block τρέχει ΜΟΝΟ μια φορά για να διορθώσει τις τιμές 
-    # χωρίς να αλλάξει τα keys που χρειάζονται τα άλλα modules.
-    if "force_fix_applied" not in s:
-        s["volume"] = 10000
-        s["price"] = 150.0
-        s["variable_cost"] = 100
-        s["fixed_cost"] = 450000.0
-        s["target_profit_goal"] = 170000
-        s["force_fix_applied"] = True
-        st.rerun() 
-    # --------------------------
     m = s.get("metrics", {})
     
     if "saved_scenarios" not in s:
