127d26e2df8d140cfadecd80cf6953398a8157bb
ManosV <manosv18@gmail.com>
2026-04-22 23:38:24 +0300

    Update home.py

--- ui/home.py
+++ ui/home.py
@@ -49,7 +49,9 @@ def run_home():
                         s.variable_cost = v_total
                         st.rerun()
 
-                st.number_input("Annual Volume", value=int(s.get("volume", 15000)), key="volume")
+                if "volume" not in s:
+                    s.volume = 10000
+                st.number_input("Annual Volume", key="volume")
                 
                 fc_val = st.number_input("Annual Fixed Costs ($)", value=float(s.get("fixed_cost", 450000.0)))
                 s.fixed_cost = fc_val 
