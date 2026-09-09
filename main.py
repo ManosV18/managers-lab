5ab760b61932869dbcc897d7bb5e66a25482d0c4
ManosV <manosv18@gmail.com>
2026-09-09 17:43:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -23,6 +23,22 @@ st.set_page_config(
     layout="wide",
 )
 
+# =========================================================
+# MICROSOFT CLARITY TRACKING
+# =========================================================
+
+import streamlit.components.v1 as components
+
+clarity_code = """
+<script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "yfnj17qehr");
+</script>
+"""
+components.html(clarity_code, height=0, width=0)
 
 # =========================================================
 # CORE IMPORTS
